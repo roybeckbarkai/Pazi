5aee58a5f89104f79110a66d9a4090c784a2acfe
shaked geffen <geffen2@mail.tau.ac.il>
2025-12-08 21:06:05 +0200

    finished data simulator

--- single sample visualiser.py
+++ single sample visualiser.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 # let's try to build a single intenisty map with the defult values
 qx, qy, I = Scatter2D(rg=1,
                       phi_tag_tag=-1 / 63,
-                      photon_noise_count=0.01,
+                      photon_noise_count=0,
                       pixel_count_along_detector=1000,
                       distance_to_detector=150,
                       wavelength=0.15,
